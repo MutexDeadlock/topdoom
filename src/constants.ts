2c9cd7f6f697759c1ac185021cd272e1d091a5d2
Fauler <matze-t-nrw@web.de>
2026-08-10 17:44:50 +0200

    v0.11.0

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { ThingType } from './game/thingtypes.ts';
 
 /** Shown bottom-right on the start menu. */
-export const VERSION = '0.10.1';
+export const VERSION = '0.11.0';
 
 /**
  * Set VITE_DEVMODE=true in .env.local to enable in-game debug info and dev hotkeys.

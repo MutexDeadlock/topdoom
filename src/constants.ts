609d6819a7c711b03374e92dd84376a99e9ed998
Fauler <matze-t-nrw@web.de>
2026-08-27 16:16:09 +0200

    v0.16.1

--- src/constants.ts
+++ src/constants.ts
@@ -9,7 +9,7 @@
 import { ThingType } from './game/things/doomednums.ts';
 
 /** Shown on the start menu. */
-export const VERSION = '0.16.0';
+export const VERSION = '0.16.1';
 
 /**
  * Set VITE_DEVMODE=true in .env.local to enable in-game debug info and dev hotkeys.

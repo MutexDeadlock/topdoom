5af063588e2b9431a0fa74c373d15c7ade1f1a69
Fauler <matze-t-nrw@web.de>
2026-09-11 03:56:47 +0200

    v0.19.0

--- src/constants.ts
+++ src/constants.ts
@@ -9,7 +9,7 @@
 import { ThingType } from './game/things/doomednums.ts';
 
 /** Shown on the start menu. */
-export const VERSION = '0.19.0-test1';
+export const VERSION = '0.19.0';
 
 /**
  * Set VITE_DEVMODE=true in .env.local to default the three Debug / Dev settings on. It gates
